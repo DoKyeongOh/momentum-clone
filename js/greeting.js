019fb60175c6c197202f033ff225185a35bc14e6
DESKTOP-82NRFN8\User <Dkproh@gmail.com>
2022-04-02 16:06:55 +0900

    mimot

--- js/greeting.js
+++ js/greeting.js
@@ -1,7 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const inputInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-
+ 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
