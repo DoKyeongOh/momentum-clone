6cdad9368c8b6c56c9ede455e1e419a23db9d436
DESKTOP-82NRFN8\User <Dkproh@gmail.com>
2022-04-02 19:58:39 +0900

    add quotes text

--- js/clock.js
+++ js/clock.js
@@ -3,10 +3,11 @@ const clock = document.querySelector("h2#clock");
 
 function displayClockH2(){
     const date = new Date();
-    let hours = date.getHours().toString().padStart(2,+"0");
-    let Minutes = date.getMinutes().toString().padStart(2,+"0");
-    let seconds = date.getSeconds().toString().padStart(2,+"0");
+    let hours = String(date.getHours()).padStart(2,+"0");
+    let Minutes = String(date.getMinutes()).padStart(2,+"0");
+    let seconds = String(date.getSeconds()).padStart(2,+"0");
     clock.innerText = `${hours}:${Minutes}:${seconds}`;
 }
 
+displayClockH2();
 setInterval(displayClockH2, 1000);
