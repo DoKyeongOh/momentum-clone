2ce77ee675ce649bba55a40722ca75b083cb8cf9
DESKTOP-82NRFN8\User <Dkproh@gmail.com>
2022-04-03 23:51:50 +0900

    add todo-list in index.html, todo.js using createElement(), appendChild() and localStorage using JSON.stringify()

---
+++ js/todo.js
@@ -0,0 +1,40 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector("#todo-list");
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");     
+
+    li.appendChild(span);
+    li.appendChild(button);
+
+    button.innerText = "❌";
+    span.innerText = newToDo;
+
+    button.addEventListener("click", deleteToDo);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newToDo);
+    toDos.push(newToDo);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
